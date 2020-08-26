acb4ab7f33023e309571dfae2ea2743ed058cceb
Andrii Otroshenko <65036633+Avocado-man@users.noreply.github.com>
2020-08-26 14:19:21 +0300

    ADD: build files

---
+++ precache-manifest.e183df33670082bd2b13911cd8177e1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39baa2d2e5b81f4b56997160114a8ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c134743f976deaf8c7c9",
+    "url": "/static/css/main.6c5a361b.chunk.css"
+  },
+  {
+    "revision": "022f8eb036f78dc9b945",
+    "url": "/static/js/2.d7587ac6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7587ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c134743f976deaf8c7c9",
+    "url": "/static/js/main.193e1f16.chunk.js"
+  },
+  {
+    "revision": "8e8e2ef8b181d889d494",
+    "url": "/static/js/runtime-main.2cca9715.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/static/media/6.c80394c2.jpg"
+  }
+]);
